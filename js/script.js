12138b550418ef58493ad7a22a6ca30bd3247da3
unknown <viewersaryo123@gmail.com>
2023-11-10 08:46:35 +0700

    uupdate v1.6

--- js/script.js
+++ js/script.js
@@ -14,3 +14,39 @@ document.addEventListener('click' , function(e){
         navbarNav.classList.remove('active');
     }
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const popcat = document.getElementById("popcat");
+  const scoreDisplay = document.getElementById("score");
+  const popSound = document.getElementById("popSound");
+
+  let score = 0;
+  let isPlaying = true;
+  let isSecondImage = false;
+
+  function toggleImage() {
+    if (isSecondImage) {
+      popcat.src = "image/gambar-1.png"; // Ganti dengan path gambar pertama
+      isSecondImage = false;
+    } else {
+      popcat.src = "image/gambar-2.png"; // Ganti dengan path gambar kedua
+      isSecondImage = true;
+    }
+  }
+
+  popcat.addEventListener("click", () => {
+    if (isPlaying) {
+      score++;
+      scoreDisplay.textContent = score;
+
+      toggleImage(); // Ganti gambar saat diklik
+      setTimeout(toggleImage, 100); // Ganti kembali setelah setengah detik
+    }
+  });
+});
+// Menambahkan event listener untuk klik pada Popcat
+popcat.addEventListener("click", () => {
+    // Memutar suara ketika Popcat diklik
+    popSound.play();
+});
