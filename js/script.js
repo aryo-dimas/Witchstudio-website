1003a4cacb95d980dfb01115cab8b912fa742747
unknown <viewersaryo123@gmail.com>
2023-11-27 08:25:49 +0700

    update 3.0

--- js/script.js
+++ js/script.js
@@ -1,169 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const videoElement = document.getElementById("background-video");
-  const navbarNav = document.querySelector(".navbar-nav");
-
-  document.querySelector("#hamburger-menu").onclick = () => {
-    navbarNav.classList.toggle("active");
-  };
-
-  const hamburger = document.querySelector("#hamburger-menu");
-
-  document.addEventListener("click", function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
-      navbarNav.classList.remove("active");
-    }
+// https://bytewebster.com/
+// https://bytewebster.com/
+// https://bytewebster.com/
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+    },
+    spaceBetween: 60,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
   });
-
-  const popcat = document.getElementById("popcat");
-  const scoreDisplay = document.getElementById("score");
-  const popSound = document.getElementById("popSound");
-  const popUpContainer = document.getElementById("popUpContainer");
-  const closePopUp = document.getElementById("closePopUp");
-  const backgroundMusic = document.getElementById("background-music");
-  const backgroundAlert = document.getElementById("background-alert");
-
-  let score = 0;
-  let isPlaying = true;
-  let isSecondImage = false;
-
-  function toggleImage() {
-    if (isSecondImage) {
-      popcat.src = "image/gambar-1.png";
-      isSecondImage = false;
-    } else {
-      popcat.src = "image/gambar-2.png";
-      isSecondImage = true;
-    }
-  }
-
-  popcat.addEventListener("click", () => {
-    if (isPlaying) {
-      score++;
-      scoreDisplay.textContent = score;
-
-      toggleImage();
-      setTimeout(toggleImage, 100);
-    }
-
-    // Memutar suara ketika Popcat diklik
-    popSound.play();
-
-    // Tampilkan popup setelah 10 klik dengan animasi
-    if (score === 10) {
-      popUpContainer.style.display = "block";
-      backgroundAlert.play();
-
-      // Tambahkan kelas "show" setelah sedikit waktu untuk memberi efek animasi
-      setTimeout(() => {
-        popUpContainer.classList.add("show");
-      }, 50);
-    }
-
-    // Periksa apakah skor mencapai 20
-    if (score >= 20 && !videoElement.hasAttribute("data-played")) {
-      videoElement.src = "webem/polish_cow.webm"; // Ganti dengan path ke video baru
-
-      // Putar lagu baru ketika latar belakang berganti
-      backgroundMusic.play();
-
-      videoElement.setAttribute("data-played", "true");
-    }
-  });
-
-  // Tambahkan event listener untuk menutup popup dengan animasi
-  closePopUp.addEventListener("click", function (event) {
-    popUpContainer.classList.remove("show");
-
-    setTimeout(() => {
-      popUpContainer.style.display = "none";
-    }, 500);
-  });
-
-  // Tambahkan event listener untuk menutup popup saat mengklik di luar popup
-  popUpContainer.addEventListener("click", function (event) {
-    if (event.target === popUpContainer) {
-      popUpContainer.classList.remove("show");
-
-      // Hapus kelas "show" setelah sedikit waktu untuk memberi efek animasi
-      setTimeout(() => {
-        popUpContainer.style.display = "none";
-      }, 500);
-    }
-  });
-
-  // Scroll Slide bar
-  const slider = document.querySelector(".card-slider");
-  let isDragging = false;
-  let startPosition = 0;
-  let currentTranslate = 0;
-  let prevTranslate = 0;
-  let animationId;
-
-  // Tentukan indeks awal slider (misalnya, indeks ke-5555)
-  const startIndex = 5555;
-  const initialTranslate = -startIndex * 100;
-
-  // Inisialisasi posisi awal
-  currentTranslate = initialTranslate;
-  slider.style.transform = `translateX(${initialTranslate}%)`;
-
-  // Event Listeners
-  slider.addEventListener("mousedown", startDragging);
-  slider.addEventListener("touchstart", startDragging);
-  slider.addEventListener("mouseup", endDragging);
-  slider.addEventListener("touchend", endDragging);
-  slider.addEventListener("mouseleave", endDragging);
-  slider.addEventListener("mousemove", drag);
-  slider.addEventListener("touchmove", drag);
-
-  function startDragging(e) {
-    if (e.type === "touchstart") {
-      startPosition = e.touches[0].clientX;
-    } else {
-      startPosition = e.clientX;
-    }
-    isDragging = true;
-    animationId = requestAnimationFrame(animation);
-    slider.style.transition = "none";
-  }
-
-  function endDragging() {
-    isDragging = false;
-    cancelAnimationFrame(animationId);
-    const movedBy = currentTranslate - prevTranslate;
-
-    if (movedBy < -100 && currentTranslate !== 0) {
-      // Swipe to the left
-      currentTranslate += 100;
-    } else if (
-      movedBy > 100 &&
-      currentTranslate !== -slider.offsetWidth * (slider.children.length - 1)
-    ) {
-      // Swipe to the right
-      currentTranslate -= 100;
-    }
-
-    slider.style.transform = `translateX(${currentTranslate}%)`;
-    slider.style.transition = "transform 0.5s ease";
-  }
-
-  function drag(e) {
-    if (isDragging) {
-      let currentPosition;
-      if (e.type === "touchmove") {
-        currentPosition = e.touches[0].clientX;
-      } else {
-        currentPosition = e.clientX;
-      }
-      const diff = currentPosition - startPosition;
-      currentTranslate = prevTranslate + (diff / slider.offsetWidth) * 100;
-    }
-  }
-
-  function animation() {
-    if (isDragging) {
-      requestAnimationFrame(animation);
-    }
-    slider.style.transform = `translateX(${currentTranslate}%)`;
-  }
+  
 });
---
+++ js/snake.js
@@ -0,0 +1,128 @@
+const playBoard = document.querySelector(".play-board");
+const scoreElement = document.querySelector(".score");
+const highScoreElement = document.querySelector(".high-score");
+const controls = document.querySelectorAll(".controls i");
+
+const section = document.querySelector("section"),/* Pop up */
+  overlay = document.querySelector(".overlay"),
+  closeBtn = document.querySelector(".close-btn");
+
+let gameOver = false;
+let foodX, foodY;
+let snakeX = 5,
+  snakeY = 5;
+let velocityX = 0,
+  velocityY = 0;
+let snakeBody = [];
+let setIntervalId;
+let score = 0;
+
+// Getting high score from the local storage
+let highScore = localStorage.getItem("high-score") || 0;
+highScoreElement.innerText = `High Score: ${highScore}`;
+
+const updateFoodPosition = () => {
+  // Passing a random 1 - 30 value as food position
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+};
+
+const handleGameOver = () => {
+  clearInterval(setIntervalId);
+
+  // Showing Popup 1
+  section.classList.add("active");
+
+  // Displaying game over message in the popup
+  const popupMessage = document.querySelector(".modal-box h2");
+  popupMessage.innerText = "Congratulation.";
+
+  // Reloading the game on OK button click
+  const okButton = document.querySelector(".buttons .close-btn");
+  okButton.addEventListener("click", () => {
+    location.reload();
+  });
+};
+
+const changeDirection = (e) => {
+  // Changing velocity value based on key press
+  if (e.key === "ArrowUp" && velocityY != 1) {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (e.key === "ArrowDown" && velocityY != -1) {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (e.key === "ArrowLeft" && velocityX != 1) {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (e.key === "ArrowRight" && velocityX != -1) {
+    velocityX = 1;
+    velocityY = 0;
+  }
+};
+
+// Calling changeDirection on each key click and passing key dataset value as an object
+controls.forEach((button) =>
+  button.addEventListener("click", () =>
+    changeDirection({ key: button.dataset.key })
+  )
+);
+
+const initGame = () => {
+  if (gameOver) return handleGameOver();
+  let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+
+  // Checking if the snake hit the food
+  if (snakeX === foodX && snakeY === foodY) {
+    updateFoodPosition();
+    snakeBody.push([foodY, foodX]); // Pushing food position to snake body array
+    score++; // increment score by 1
+    highScore = score >= highScore ? score : highScore;
+    localStorage.setItem("high-score", highScore);
+    scoreElement.innerText = `Score: ${score}`;
+    highScoreElement.innerText = `High Score: ${highScore}`;
+  }
+  // Updating the snake's head position based on the current velocity
+  snakeX += velocityX;
+  snakeY += velocityY;
+
+  // Shifting forward the values of the elements in the snake body by one
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+  snakeBody[0] = [snakeX, snakeY]; // Setting first element of snake body to current snake position
+
+  // Checking if the snake's head is out of wall, if so setting gameOver to true
+  if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+    return (gameOver = true);
+  }
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    // Adding a div for each part of the snake's body
+    html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+    // Checking if the snake head hit the body, if so set gameOver to true
+    if (
+      i !== 0 &&
+      snakeBody[0][1] === snakeBody[i][1] &&
+      snakeBody[0][0] === snakeBody[i][0]
+    ) {
+      gameOver = true;
+    }
+  }
+  playBoard.innerHTML = html;
+};
+
+updateFoodPosition();
+setIntervalId = setInterval(initGame, 100);
+document.addEventListener("keyup", changeDirection);
+
+// Pop up 1
+overlay.addEventListener("click", () => section.classList.remove("active"));
+closeBtn.addEventListener("click", () => section.classList.remove("active"));
+function openFile() {
+  // Ganti URL_FILE dengan URL yang sesuai
+  const fileUrl = "https://www.youtube.com/watch?v=b5Ajtqqu2eA";
+
+  // Membuka link dalam tab baru
+  window.open(fileUrl, "_blank");
+}
